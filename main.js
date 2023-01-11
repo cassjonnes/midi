325d94a1e342244450178adc45f2085f19e02433
cassjonnes <cass.mollona@gmail.com>
2023-01-11 11:24:00 -0300

    Alteracao while para for

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*
+
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
@@ -16,21 +16,4 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
 function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
-}
-*/
-
-function tocaSom (idAudio){
-    document.querySelector(idAudio).play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
-
-while (contador < 9) {
-    let instrumento = listaDeTeclas[contador].classList[1];
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom(`#som_${instrumento}`);
-    }
-    contador = contador +1;
-    console.log(contador);
 }
